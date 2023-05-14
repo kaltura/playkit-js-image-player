07aff54221b154204352967e5a581c80373379b1
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2023-05-14 16:06:27 +0100

    fix(FEC-13123): replace travis with github actions (#15)
    solves FEC-13123

--- tests/e2e/image-player.spec.js
+++ tests/e2e/image-player.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import '../../src/index';
-import { setup } from 'kaltura-player-js';
+import { setup } from '@playkit-js/kaltura-player-js';
 import { config, targetId } from '../mock/config';
 import { mediaData } from '../mock/media-sourc';
 
